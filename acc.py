60e4cc4106c72d1bc746f9eedd78ecf52d2aa8b5
francnstein <55936196+francnstein@users.noreply.github.com>
2020-01-02 18:58:10 -0500

    Update acc.py

--- acc.py
+++ acc.py
@@ -1,7 +1,7 @@
 
 
-account_sid = 'ACc60fb62704186ab042d36aaa183fe72d'
-auth_token = '8cb2b839a8e03a146400a9b4610df177'
-tel = '4079077093'
-twil = '+19727462931'
+account_sid = 'your sid'
+auth_token = 'your token'
+tel = 'your num'
+twil = 'your twilio num'
 
